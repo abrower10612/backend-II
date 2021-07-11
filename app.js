d12349a4758c38a9da253b6615bf365f8252600a
abrower10612 <abrower7@byui.edu>
2021-07-10 20:15:22 -0600

    heroku

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// If you downloaded this code, make sure you run `npm install`
-// before trying to start the app.
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -19,10 +17,9 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .use(
     session({
-      // Simple and not very secure session
       secret: 'random_text',
       cookie: {
-        httpOnly: false, // Permit access to client session
+        httpOnly: false,
       },
     })
   )
@@ -39,9 +36,7 @@ io.on('connection', (socket) => {
       console.log('A client disconnected!');
     })
     .on('newUser', (username, time) => {
-      // A new user logs in.
-      const message = `${username} has logged on.`;
-      // Tell other users someone has logged on.
+      const message = `${username} has entered the chat.`;
       socket.broadcast.emit('newMessage', {
         message,
         time,
@@ -49,13 +44,8 @@ io.on('connection', (socket) => {
       });
     })
     .on('message', (data) => {
-      // Receive a new message
-      console.log('Message received');
-      console.log(data);
-      // This one is simple. Just broadcast the data we received.
-      // We can use { ...data } to copy the data object.
       socket.broadcast.emit('newMessage', {
         ...data,
-      }); // Note, only emits to all OTHER clients, not sender.
+      });
     });
 });
